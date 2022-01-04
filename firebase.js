43733a0f9bb240e2a37df04d5a6f2a92f521768f
Vivek Darak <43067197+bugfisher@users.noreply.github.com>
2022-01-04 17:47:31 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,14 +7,7 @@ import {
 import { getStorage } from "firebase/storage";
 import { initializeFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAW2yVxR3s5EdnGGWmQdGwwhIzXXUJH3xw",
-  authDomain: "wp-clone-7f3ae.firebaseapp.com",
-  projectId: "wp-clone-7f3ae",
-  storageBucket: "wp-clone-7f3ae.appspot.com",
-  messagingSenderId: "43020098328",
-  appId: "1:43020098328:web:1c56f5cf866d1b72c65c6d"
-};
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
